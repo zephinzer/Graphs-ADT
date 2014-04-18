0a6842db322d6d4ccd00e7af73107cb54c4ce731
zephinzer <zephinzer@gmail.com>
2014-04-18 13:26:46 +0800

    Put source files into 'src' folder and configured bar.bat to output binaries to 'bin' folder before running

--- bar.bat
+++ bar.bat
@@ -1,4 +1,5 @@
 echo Build and run Graphs
-g++ graphs.cpp -o graphs -static-libgcc -static-libstdc++ -s -Os
+g++ ./src/graphs.cpp -o ./bin/graphs -static-libgcc -static-libstdc++ -s -Os
+cd bin
 graphs
 pause
