bc051e3a2cf3749b40b577ac9693ced657f1fe32
zephinzer <zephinzer@gmail.com>
2014-04-18 13:23:04 +0800

    added graphs.h to git

---
+++ graphs.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef std::string String;
+
+// enum
+#include "direction.h"
+
+// classes
+#include "node.h"
+#include "edge.h"
+#include "graph.h"
